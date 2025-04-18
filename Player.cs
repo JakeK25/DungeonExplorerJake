b4a87d0bc4facbaa69cd10ca65db6e9878aebad8
jakekeightley <28997203@students.lincoln.ac.uk>
2025-04-18 22:15:10 +0100

    Added more functionality and moving rooms and removed some unuseable code

--- Player.cs
+++ Player.cs
@@ -1,59 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.InteropServices;
 
 namespace DungeonExplorer
 {
-    public class Item                              // Class constructor item, creating the item for the user to pick-up
+    public class Item // Class constructor item, creating the item for the user to pick-up
     {
-        public string name;
         public string desc;
+        public string name;
+
         public Item(string _name, string _desc)
         {
             name = _name;
             desc = _desc;
         }
     }
-    public class Player                                     // Class constructor Player
-    {
-        public int LevelNumber = 1;
-        public string Name { get; private set; }
-        public int Health { get; private set; }
-        private List<Item> inventory = new List<Item>();
-        public Room CurrentRoom;
+    public class Player                                   // Class constructor Player
+    { 
+        public void MoveRoomChoice()
+        {
+            Console.WriteLine(
+                "Which room would you like to enter? \n 1. Spiders Den (S) \n 2. Hornets Lair (H) \n 3. Crab Infested Lair (C) \n 4. Undead Pirate Cavern (U) \n 5. Navy Soldier's Armory (N) \n 6. Phantom's Graveyard (P) \n 7. BOSS ROOM! (B)");
+            var userInput = Console.ReadLine();
 
-        public Player(int health)                                        // Create health system and gather input from the user for username
+            if (userInput == "1")
+            {
+                var spidersDen =
+                    new Room("Spider's Den \n You walk into a large cave covered in cobwebs, and you feel a unnerving feeling...");
+                this.CurrentRoom = spidersDen;
+            }
+            else if (userInput == "2")
+            {
+                var hornetsNest =
+                    new Room("Hornet's Nest \n You walk into a large cave with honeycombed walls, the floor is sticky and you hear a buzzing noise in the distance...");
+            }
+            else if (userInput == "3")
+            {
+                var crabLair =
+                    new Room("Crab Infested Lair \n You walk into a cave with a thin layer of water along the floor and a scuttering sound in the distance...");
+            }
+            else if (userInput == "4")
+            {
+                var pirateCavern =
+                    new Room("Undead Pirate Cavern \n You walk into a cave scattered with pirates crawling along the floor with flesh missing from their bodies you immediately feel a sense of danger...");
+            }
+            else if (userInput == "5")
+            {
+                var navyRoom =
+                    new Room("Navy Soldier's Armory \n You are trespassing in a navy soldier armory with soldiers loitering and armoring up, they all turn to look at you in confusion...");
+            }
+            else if (userInput == "6")
+            {
+                var phantomRoom =
+                    new Room("Phantom's Graveyard \n You walk into a gloomy graveyard, fog covers your sight and you hear shrieking in the distance... ");
+            }
+            else if (userInput == "7")
+            {
+                var bossRoom = new Room("Boss Room \n Placeholder");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong... Please enter a number between 1 and 7...");
+            }
+        }
+        
+        
+        public Room CurrentRoom;
+        private readonly List<Item> inventory = new List<Item>();
+        public int LevelNumber = 1;
+
+        public Player(int health) // Create health system and gather input from the user for username
         {
             inventory = new List<Item>();
             Health = health;
             Console.WriteLine("What would you like your username to be?");
-            String Name = Console.ReadLine();
+            var Name = Console.ReadLine();
             Console.WriteLine("Your username is: " + Name);
-                      
         }
+
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+
         public void Damage(int damage)
         {
             Health = Health - damage;
         }
-        public void ShowHealth() 
+
+        public void ShowHealth()
         {
             Console.WriteLine("Health is currently:" + Health);
-        } 
-        public void PickUpItem(Item item)                           // Pick-up item add to inventory
+        }
+
+        public void PickUpItem(Item item) // Pick-up item add to inventory
         {
             inventory.Add(item);
             InventoryContents();
         }
+
         public void InventoryContents()
         {
-            Console.Write("Your current inventory is: ");                 // Display inventory to the user
+            Console.Write("Your current inventory is: "); // Display inventory to the user
 
-            foreach (Item i in inventory)
-            {
-                Console.Write("" + i.name +" ");
-            }
+            foreach (var i in inventory) Console.Write("" + i.name + " ");
+
             Console.Read();
+        }
+
+        public abstract class Creature
+        {
+            protected int health;
+            protected string name;
+
+            protected Creature(string _name, int _health)
+            {
+                name = _name;
+                health = _health;
+            }
+        }
+
+        public class Monster : Creature
+        {
+            public Monster() : base("Monster", 500)
+            {
+            }
+        }
+
+        public class Boss : Creature
+        {
+            public Boss() : base("Boss", 1500)
+            {
+            }
         }
     }
 }
--- Room.cs
+++ Room.cs
@@ -1,19 +1,116 @@
 ﻿using System;
-using System.Diagnostics.Eventing.Reader;
 
 namespace DungeonExplorer
 {
     public class Room
+
     {
-        // Test
-        private string description;
+        private readonly string description;
 
         public Room(string description)
         {
             this.description = description;
         }
 
-        public string GetDescription() { return this.description;  }        // Encapsulate room description 
-            
+        public string GetDescription()
+        {
+            return description;
+        } // Encapsulate room description 
     }
-} 
+
+    public class Game_Map
+    {
+        private readonly int height;
+        private readonly char[,] map;
+        private readonly int width;
+
+        public Game_Map(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            map = new char[width, height];
+
+            for (var y = 0; y < height; y++)
+            for (var x = 0; x < width; x++)
+                map[x, y] = '.';
+
+            SetMapChar(13, 0, '\u2693'); // Displays start of map
+
+            // Map Paths
+
+            SetMapChar(12, 0, '\u2199'); // SW           
+            SetMapChar(13, 1, '\u2193'); // S
+            SetMapChar(11, 1, '\u2199'); // SW
+            SetMapChar(13, 2, '\u2193'); // S
+            SetMapChar(11, 9, '\u2199'); // SW
+            SetMapChar(10, 9, '\u2190'); // W
+            SetMapChar(12, 5, '\u2193'); // S
+            SetMapChar(12, 6, '\u2193'); // S
+            SetMapChar(9, 9, '\u2190'); // W
+            SetMapChar(8, 9, '\u2190'); // W
+            SetMapChar(7, 9, '\u2190'); // W
+            SetMapChar(5, 9, '\u2190'); // W
+            SetMapChar(4, 9, '\u2190'); // W
+            SetMapChar(3, 9, '\u2190'); // W
+            SetMapChar(2, 9, '\u2190'); // W
+            SetMapChar(8, 2, '\u2190'); // W
+            SetMapChar(7, 2, '\u2190'); // W
+            SetMapChar(4, 3, '\u2199'); // SW
+            SetMapChar(3, 4, '\u2199'); // SW
+            SetMapChar(1, 6, '\u2193'); // S
+            SetMapChar(1, 7, '\u2193'); // S
+
+            // Room Icons
+
+            SetMapChar(10, 1, 'S'); // Displays Spiders room
+            SetMapChar(10, 2, 'S');
+            SetMapChar(9, 1, 'S');
+            SetMapChar(9, 2, 'S');
+            SetMapChar(13, 3, 'H'); // Displays Hornets room
+            SetMapChar(13, 4, 'H');
+            SetMapChar(12, 3, 'H');
+            SetMapChar(12, 4, 'H');
+            SetMapChar(12, 7, 'U'); // Displays Undead pirates room
+            SetMapChar(12, 8, 'U');
+            SetMapChar(13, 7, 'U');
+            SetMapChar(13, 8, 'U');
+            SetMapChar(6, 9, 'P'); // Displays Phantoms room
+            SetMapChar(5, 9, 'P');
+            SetMapChar(6, 8, 'P');
+            SetMapChar(5, 8, 'P');
+            SetMapChar(6, 2, 'C'); // Displays Crab infested room
+            SetMapChar(5, 2, 'C');
+            SetMapChar(6, 1, 'C');
+            SetMapChar(5, 1, 'C');
+            SetMapChar(2, 4, 'N'); // Undead Navy soldier room
+            SetMapChar(1, 4, 'N');
+            SetMapChar(2, 5, 'N');
+            SetMapChar(1, 5, 'N');
+            SetMapChar(0, 9, 'B'); // Displays final boss battle (End of Map)
+            SetMapChar(1, 9, 'B');
+            SetMapChar(0, 8, 'B');
+            SetMapChar(1, 8, 'B');
+        }
+
+        private void SetMapChar(int x, int y, char character)
+        {
+            if (x >= 0 && x < width && y >= 0 && y < height)
+                map[x, y] = character;
+            else
+                Console.WriteLine("Invalid Character");
+        }
+
+        public void DisplayMap()
+        {
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++) Console.Write(map[x, y] + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("enter to continue");
+            Console.Read();
+        }
+    }
+}
